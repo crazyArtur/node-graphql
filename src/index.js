7d0b5341326b23b20db1f1eee342652d4b87ca6d
crazyArtur <artur.dbsz@gmail.com>
2020-02-13 22:22:20 +1100

    NQ | Simple Query - updated types in schema and added resolvers

--- src/index.js
+++ src/index.js
@@ -3,12 +3,33 @@ const { GraphQLServer } = require("graphql-yoga");
 const typeDefs = `
 type Query {
     info: String!
+    feed: [Link!]!
+}
+
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `;
 
+let links = [
+  {
+    id: "link-0",
+    description: "Fullstack TT for GQL",
+    url: "www.howtographql.com"
+  }
+];
+
 const resolvers = {
   Query: {
-    info: () => `This is the API of a HN Clone`
+    info: () => `This is the API of a HN Clone`,
+    feed: () => links
+  },
+  Link: {
+    id: parent => parent.id,
+    description: parent => parent.description,
+    url: parent => parent.url
   }
 };
 

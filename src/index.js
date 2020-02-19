32e83104dc0eb85a0fe7b922cd344f2fc2b8d3a5
crazyArtur <artur.dbsz@gmail.com>
2020-02-19 16:50:00 +1100

    NQ | Added authentication resolvers

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ const { GraphQLServer } = require('graphql-yoga')
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
-        feed: (root, args, context, info) => {
-            return context.prisma.links()
-        },
+
     },
     Mutation: {
         post: (root, args, context) => {
@@ -22,6 +20,11 @@ const resolvers = {
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
-    context: { prisma },
+    context: request => {
+        return {
+            ...request,
+            prisma,
+        }
+    },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))

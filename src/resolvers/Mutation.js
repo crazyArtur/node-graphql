df187cb092983b05f93e935caa6b08c97e527b19
crazyArtur <artur.dbsz@gmail.com>
2020-02-20 18:31:01 +1100

    BugFix | Mutations

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 const { APP_SECRET, getUserId } = require('../utils')
 
 async function signup(parent, args, context, info) {
-    const password = await bcrypt.hash(args, password, 10)
+    const password = await bcrypt.hash(args.password, 10)
     const user = await context.prisma.createUser({ ...args, password })
     const token = jwt.sign({ userId: user.id }, APP_SECRET)
 

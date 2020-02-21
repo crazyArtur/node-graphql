37a3eee3190e8961eb23ec00e8c79baea9c1d11d
crazyArtur <artur.dbsz@gmail.com>
2020-02-21 15:08:17 +1100

    NQ | Added subscription for votes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
 const Subscription = require('./resolvers/Subscription')
+const Vote = require('./resolvers/Vote')
 
 const { prisma } = require('./generated/prisma-client')
 const { GraphQLServer } = require('graphql-yoga')
@@ -12,7 +13,8 @@ const resolvers = {
     Mutation,
     Subscription,
     User,
-    Link
+    Link,
+    Vote,
 }
 
 // 3
